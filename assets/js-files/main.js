bdeddb3aafd6a68bf557e40b155867f583364a7f
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-02-16 00:53:34 +0000

    adjusted the gsap scrollTrigger to be responsive to any section size gets fed to it further in development

--- assets/js-files/main.js
+++ assets/js-files/main.js
@@ -3,14 +3,20 @@
 
  let navLinks = document.querySelectorAll('.nav-spy-wrapper > ul > *')
  for (let navLink of navLinks) {
+     let sectionClass = '.' + navLink.className + '-section'
+
      gsap.to(navLink, {
          scrollTrigger: {
              trigger: '#' + navLink.className,
              toggleActions: "play reverse play reverse",
-             start: 'top 50%',
-             end: 'bottom -2%',
+             start: 'top 40%',
+             endTrigger: sectionClass,
+             end: 'bottom 40%',
+             markers: true
          },
          fontSize: "30",
          duration: 1,
      })
  }
+
+ // do the end with the size of the section e voilla
