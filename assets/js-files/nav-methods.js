3e73577cd93c4e12fef569dae28a4c017faed935
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-02-21 19:46:29 +0000

    refactored gsap scrolltrigger to create an object that takes timelines with more selectors, plaied with the mobile navigation arrow selectors

--- assets/js-files/nav-methods.js
+++ assets/js-files/nav-methods.js
@@ -8,19 +8,23 @@ let navSystem = function () {
     let navLinkLiElements = document.querySelectorAll('.nav-spy-wrapper > div > li')
     let svgMobileSelector = document.querySelector('.mobile-nav-selector')
     let svgDesktopSelector = document.querySelector('.profile-svg')
-
+    let svgMobileSelectorUpperArrow = document.querySelector('.upper-mobile-svg-arrow')
+    let svgMobileSelectorLowerArrow = document.querySelector('.lower-mobile-svg-arrow')
     for (let navLi of navLinkLiElements) {
         let sectionClass = '.' + navLi.className.replace('-link-li', '-section')
-        gsap.to(navLi, {
-            scrollTrigger: {
-                trigger: sectionClass,
-                toggleActions: "play reverse play reverse",
-                start: 'top 50%',
-                end: 'bottom 50%',
-            },
+        const resizeTl = gsap.timeline()
+        resizeTl.to(navLi, {
             fontSize: "2em",
             color: "#FF2156",
-            duration: 0.2,
+            duration: 0.5,
+        })
+
+        ScrollTrigger.create({
+            animation: resizeTl,
+            trigger: sectionClass,
+            toggleActions: "play reverse play reverse",
+            start: 'top 50%',
+            end: 'bottom 50%',
         })
 
         let selectorObserver = new ResizeObserver(() => {
@@ -31,15 +35,35 @@ let navSystem = function () {
                 let liOffsetX = navLi.offsetLeft + liMiddle - svgMobileSelectorWidth
                 let liOffsetY = navLi.offsetTop
                 let selectorXCoor = liOffsetX
-
-                gsap.to(svgMobileSelector, {
+                const selectorTl = gsap.timeline()
+                selectorTl.to(svgMobileSelector, {
                     x: selectorXCoor,
-                    duration: 1
+                    duration: 0.5,
                 })
+                selectorTl.to(svgMobileSelectorUpperArrow, {
+                    x: -30,
+                    duration: 0.5,
+                }, '-=0.4')
+                selectorTl.to(svgMobileSelectorLowerArrow, {
+                    x: 30,
+                    duration: 0.5,
+
+                }, '<')
+                selectorTl.to(svgMobileSelectorUpperArrow, {
+                    x: 0,
+                    duration: 0.7,
+                    ease: Elastic.easeOut,
+                }, '-=0.25')
+                selectorTl.to(svgMobileSelectorLowerArrow, {
+                    x: 0,
+                    duration: 0.5,
+                    ease: Elastic.easeOut,
+                }, '<')
+
                 let liDegree = liOffsetY
                 gsap.to(svgDesktopSelector, {
                     rotate: liDegree + 10,
-                    duration: 0.5
+                    duration: 0.2
                 })
             }
         })
