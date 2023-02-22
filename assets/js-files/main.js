c1d84b5b0a16ea031325ad1c7725c98d3846b2e0
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-02-22 17:33:05 +0000

    reworked the navigation js file and added loading functionality in the main.js file

--- assets/js-files/main.js
+++ assets/js-files/main.js
@@ -1,5 +1,50 @@
 import {
-    navSystem
+    navSystem,
+    selectorsAnimations
 } from './nav-methods.js'
 
-navSystem()
+const navSelectors = {
+    profileImmage: document.querySelector('#profile-immage'),
+    navLinkLiElements: document.querySelectorAll('.nav-spy-wrapper > div > li'),
+    svgMobileSelector: document.querySelector('.mobile-nav-selector'),
+    svgDesktopSelector: document.querySelector('.profile-svg'),
+    svgMobileSelectorUpperArrow: document.querySelector('.upper-mobile-svg-arrow'),
+    svgMobileSelectorLowerArrow: document.querySelector('.lower-mobile-svg-arrow')
+}
+const master = new TimelineMax()
+// loadNav
+const loadNavBar = function (selectors) {
+    const loadTl = new TimelineMax({
+        defaults: {
+            ease: 'ease-in',
+            duration: 1
+        }
+    })
+    for (let navli of selectors.navLinkLiElements) {
+        loadTl.from(navli, {
+            opacity: 0,
+            x: -100,
+        }, '<')
+    }
+    loadTl.from(navSelectors.profileImmage, {
+        opacity: 0,
+        y: -100,
+        duration: 0.4,
+    })
+    loadTl.from(selectors.svgDesktopSelector, {
+        opacity: 0,
+    })
+    loadTl.from(selectors.svgMobileSelector, {
+        opacity: 0,
+        x: 30,
+        duration: 1,
+        ease: Elastic.easeOut,
+    }, '<')
+    return loadTl
+}
+navSystem(navSelectors)
+master.add(loadNavBar(navSelectors), )
+
+setTimeout(() => {
+    selectorsAnimations(navSelectors)
+}, 1500)
--- assets/js-files/nav-methods.js
+++ assets/js-files/nav-methods.js
@@ -1,94 +1,85 @@
-let navSystem = function () {
+const navSystem = function (selectors) {
     /** 
      *creates the navigation menu animations for desktop and mobile versions with the gsap library
      */
-
     gsap.registerPlugin(ScrollTrigger)
 
-    let navLinkLiElements = document.querySelectorAll('.nav-spy-wrapper > div > li')
-    let svgMobileSelector = document.querySelector('.mobile-nav-selector')
-    let svgDesktopSelector = document.querySelector('.profile-svg')
-    let svgMobileSelectorUpperArrow = document.querySelector('.upper-mobile-svg-arrow')
-    let svgMobileSelectorLowerArrow = document.querySelector('.lower-mobile-svg-arrow')
-    let profileDesktopImage = document.querySelector('#profile-immage')
-    for (let navLi of navLinkLiElements) {
-        // over-engineering execrcice */
-        //let angle = findRotationAngle(navLi.firstChild, profileDesktopImage)
-        let sectionClass = '.' + navLi.className.replace('-link-li', '-section')
-        const resizeTl = gsap.timeline()
-        resizeTl.to(navLi, {
-            fontSize: "2em",
-            color: "#FF2156",
-            duration: 0.5,
-        })
-        ScrollTrigger.create({
-            animation: resizeTl,
-            trigger: sectionClass,
-            toggleActions: "play reverse play reverse",
-            start: 'top 50%',
-            end: 'bottom 50%',
-        })
+    for (let navLi of selectors.navLinkLiElements) {
+        changeNavliSize(navLi)
+    }
+}
 
-        let selectorObserver = new ResizeObserver(() => {
+const changeNavliSize = function (navLi) {
+    let sectionClass = '.' + navLi.className.replace('-link-li', '-section')
+    const resizeTl = new TimelineMax()
+    resizeTl.to(navLi, {
+        fontSize: "2em",
+        color: "#FF2156",
+        duration: 0.5,
+    })
+    return ScrollTrigger.create({
+        animation: resizeTl,
+        trigger: sectionClass,
+        toggleActions: "play reverse play reverse",
+        start: 'top 50%',
+        end: 'bottom 50%',
+    })
+}
+
+const selectorsAnimations = function (selectors) {
+    for (let navLi of selectors.navLinkLiElements) {
+        const selectorObserver = new ResizeObserver(() => {
+
             if ((navLi.style.fontSize) == '2em') {
-
                 let liMiddle = navLi.offsetWidth / 2
                 let svgMobileSelectorWidth = 7.5
                 let liOffsetX = navLi.offsetLeft + liMiddle - svgMobileSelectorWidth
                 let liOffsetY = navLi.offsetTop
                 let selectorXCoor = liOffsetX
-                const selectorTl = gsap.timeline()
-                selectorTl.to(svgMobileSelector, {
-                    x: selectorXCoor,
-                    duration: 0.4,
-                })
-                selectorTl.to(svgMobileSelectorUpperArrow, {
-                    x: -30,
-                    duration: 0.5,
-                }, '-=0.3')
-                selectorTl.to(svgMobileSelectorLowerArrow, {
-                    x: 30,
-                    duration: 0.5,
+                let tl = new TimelineMax()
+                if (window.innerWidth < 800) {
+                    tl.to(selectors.svgMobileSelector, {
+                        x: selectorXCoor,
+                        duration: 0.4,
+                    }, )
+                    tl.to(selectors.svgMobileSelectorUpperArrow, {
+                        x: 30,
+                        duration: 0.4,
+                    }, '-=0.3')
+                    tl.to(selectors.svgMobileSelectorLowerArrow, {
+                        x: 30,
+                        duration: 0.4,
 
-                }, '<')
-                selectorTl.to(svgMobileSelectorUpperArrow, {
-                    x: 0,
-                    duration: 0.7,
-                    ease: Elastic.easeOut,
-                }, '-=0.25')
-                selectorTl.to(svgMobileSelectorLowerArrow, {
-                    x: 0,
-                    duration: 0.3,
-                    ease: Elastic.easeOut,
-                }, '<')
-                let percentageDegree = liOffsetY < 100 ? 0.15 : 0.27
-                let liDegree = liOffsetY - percentageDegree * liOffsetY
-                gsap.to(svgDesktopSelector, {
-                    rotate: liDegree + 10,
-                    duration: 0.5,
-                    ease: Elastic.easeOut.config(1.5, 0.6),
-                })
+                    }, '<')
+                    tl.to(selectors.svgMobileSelectorUpperArrow, {
+                        x: 0,
+                        duration: 0.7,
+                        ease: Elastic.easeOut,
+                    }, '-=0.25')
+                    tl.to(selectors.svgMobileSelectorLowerArrow, {
+                        x: 0,
+                        duration: 0.7,
+                        ease: Elastic.easeOut,
+                    }, '<')
+                } else {
+                    let percentageDegree = liOffsetY < 100 ? 0.15 : 0.27
+                    let liDegree = liOffsetY - percentageDegree * liOffsetY
+                    tl.to(selectors.svgDesktopSelector, {
+                        rotate: liDegree + 10,
+                        duration: 0.7,
+                        ease: Elastic.easeOut.config(1.5, 0.6),
+                    })
+                }
+                return tl
             }
         })
         selectorObserver.observe(navLi)
+
     }
+
 }
-
-// let radianToDegree = r => r * 180 * Math.PI ** -1
-
-// let findRotationAngle = function (li, svg) {
-
-//     let liRect = li.getBoundingClientRect()
-//     let svgRect = svg.getBoundingClientRect()
-
-//     let opositeSeg = liRect.x - svgRect.x - svgRect.width / 2
-//     let elevationSeg = Math.abs(svgRect.y + svgRect.height / 2 - liRect.y - liRect.width / 2)
-//     let hipotenuse = Math.sqrt(Math.pow(opositeSeg, 2) + Math.pow(elevationSeg, 2))
-//     let angle = radianToDegree(Math.asin(opositeSeg / hipotenuse).toFixed(3) * 1)
-//     console.log(angle)
-//     return angle
-// }
 
 export {
     navSystem,
+    selectorsAnimations
 }
