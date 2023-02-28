225867b00309bfe075141ec0304e8f291c5819e7
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-02-28 16:56:50 +0000

    added touch-start and touch-end events on the pdf-download-svg

--- assets/js-files/intro-section-methods.js
+++ assets/js-files/intro-section-methods.js
@@ -9,39 +9,49 @@ import {
 } from '../../node_modules/gsap/MorphSVGPlugin.js'
 gsap.registerPlugin(MorphSVGPlugin)
 
-
-let hoverDownloadPDF = function (svgElement, svgPaths) {
-    svgElement.addEventListener('mouseover', () => {
-        let tl = new TimelineMax()
-        for (let gElement of svgPaths) {
-            let startPath
-            let endPath
-            for (let path of gElement.children) {
-                if (path.getAttribute('data-name') == "start") {
-                    startPath = path
-                } else {
-                    endPath = path
-                }
+let pdfSvgMouseover = function (svgPaths) {
+    let tl = new TimelineMax()
+    for (let gElement of svgPaths) {
+        let startPath
+        let endPath
+        for (let path of gElement.children) {
+            if (path.getAttribute('data-name') == "start") {
+                startPath = path
+            } else {
+                endPath = path
             }
-            tl.to(startPath, {
-                morphSVG: endPath,
-                duration: 0.5,
+        }
+        tl.to(startPath, {
+            morphSVG: endPath,
+            duration: 0.5,
+            ease: Elastic.easeOut,
+        }, '<')
+    }
+}
+let pdfSfgMouseleave = function (svgPaths) {
+    let tl = new TimelineMax()
+    for (let gElement of svgPaths) {
+        for (let path of gElement.children) {
+            tl.to(path, {
+                morphSVG: path,
+                duration: 1,
                 ease: Elastic.easeOut,
             }, '<')
         }
-
+    }
+}
+let hoverDownloadPDF = function (svgElement, svgPaths) {
+    svgElement.addEventListener('mouseover', () => {
+        pdfSvgMouseover(svgPaths)
     })
     svgElement.addEventListener('mouseleave', () => {
-        let tl = new TimelineMax()
-        for (let gElement of svgPaths) {
-            for (let path of gElement.children) {
-                tl.to(path, {
-                    morphSVG: path,
-                    duration: 1,
-                    ease: Elastic.easeOut,
-                }, '<')
-            }
-        }
+        pdfSfgMouseleave(svgPaths)
+    })
+    svgElement.addEventListener('touchstart', () => {
+        pdfSvgMouseover(svgPaths)
+    })
+    svgElement.addEventListener('touchend', () => {
+        pdfSfgMouseleave(svgPaths)
     })
 }
 
