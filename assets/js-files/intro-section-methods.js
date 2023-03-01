df124dd57eb078f43b2066c6827a5956b64357c4
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-03-01 21:02:24 +0000

    debugging modules imports

--- assets/js-files/intro-section-methods.js
+++ assets/js-files/intro-section-methods.js
@@ -2,10 +2,12 @@ import {
     gsap,
     TimelineMax,
     Elastic
-} from '/node_modules/gsap/index.js'
+} from "/node_modules/gsap/index.js"
+
+
 import {
     MorphSVGPlugin
-} from '/node_modules/gsap/MorphSVGPlugin.js'
+} from "/node_modules/gsap/MorphSVGPlugin.js"
 
 gsap.registerPlugin(MorphSVGPlugin)
 const pdfSvgMouseover = function (svgPaths) {
