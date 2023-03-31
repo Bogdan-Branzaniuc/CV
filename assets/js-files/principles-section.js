478a9e70b56d37443a45436194c9c339f96f6e3a
Bogdan-Branzaniuc <branzaniucbogdan20@gmail.com>
2023-03-31 19:11:34 +0000

    finished principles section functionality

--- assets/js-files/principles-section.js
+++ assets/js-files/principles-section.js
@@ -17,99 +17,161 @@ import {
     InertiaPlugin
 } from '../node_modules/gsap/InertiaPlugin.js'
 
+import {
+    ScrollTrigger
+} from '../node_modules/gsap/ScrollTrigger.js'
+
 gsap.registerPlugin(MorphSVGPlugin)
 gsap.registerPlugin(Draggable)
 gsap.registerPlugin(InertiaPlugin)
+gsap.registerPlugin(ScrollTrigger)
+
 let buttons = document.querySelectorAll('.principles-buttons')
 let mainSvg = '#principles-svg'
+let activeButton = 'scope-button'
 
-function resize(button) {
+function resizeWindow(button) {
     let buttonY = button.offsetTop
     let buttonX = button.offsetLeft
-    buttonsCoordsX[i] = buttonX
-    buttonsCoordsY[i] = buttonY
     return {
         x: buttonX,
         y: buttonY
     }
 }
-
 let buttonsCoordsX = []
 let buttonsCoordsY = []
-let i = 0
+let whichY = 0
 
-for (let button of buttons) {
-    button.addEventListener('click', (e) => {
-        let buttonClassName = button.classList[2]
-        let newSvgId = '#' + buttonClassName.replace('button', 'svg')
-        changePrinciple(newSvgId)
-        snapToNewButton(e.target)
-    })
+function createCoords() {
+    buttonsCoordsX = []
+    buttonsCoordsY = []
+    let rowX = []
 
-    let btnCoords = resize(button)
+    for (let button of buttons) {
+        gsap.set('.draggable-remote', {
+            x: buttons[0].offsetLeft,
+            y: buttons[0].offsetTop
+        })
+        button.addEventListener('click', (e) => {
+            let buttonID = button.id
+            let newSvgId = '#' + buttonID.replace('button', 'svg')
+            changePrinciple(newSvgId)
+            snapToNewButton(e.target)
+        })
+        let btnCoords = resizeWindow(button)
+        let IncludedX = buttonsCoordsX.includes(btnCoords.x)
+        let IncludedY = buttonsCoordsY.includes(btnCoords.y)
+        if (!IncludedX) {
+            if (btnCoords.x < rowX[rowX.length - 1]) {
+                buttonsCoordsX.push(rowX)
+                rowX = []
+                rowX.push(btnCoords.x)
+            } else {
+                rowX.push(btnCoords.x)
+            }
+        }
+        if (!IncludedY) {
+            buttonsCoordsY.push(btnCoords.y)
+        }
+    }
+    buttonsCoordsX.push(rowX)
+    rowX = []
+}
+createCoords()
 
-    buttonsCoordsX.push(btnCoords.x)
-    buttonsCoordsY.push(btnCoords.y)
+window.addEventListener('resize', () => {
+    createCoords()
+    scrollTriggerPrinciples.refresh()
+})
+let scrollTriggerPrinciples = ScrollTrigger.create({
+    trigger: '.buttons-area-container',
+    start: "top 65%",
+    end: "bottom 40%",
+    onEnter: () => {
+        let currentButtonSvgId = '#' + activeButton.replace('button', 'svg')
+        changePrinciple(currentButtonSvgId)
+        console.log('enter')
+    },
+    onLeave: () => {
+        changePrinciple('#principles-svg')
+        console.log('leave')
+    },
+    onEnterBack: () => {
+        let currentButtonSvgId = '#' + activeButton.replace('button', 'svg')
+        changePrinciple(currentButtonSvgId)
+        console.log('enter back')
+    },
+    onLeaveBack: () => {
+        changePrinciple('#principles-svg')
+        console.log('leave back')
+    },
+})
 
-    window.addEventListener('resize', () => {
-        resize(button)
-    })
-    //draggable.isTouching
-    i++
+function proccessSnapX(endValue, whichYrow) {
+    let coordIndex = buttonsCoordsX[whichYrow].reduce((incrementIndex, coord, currentIndex, array) => {
+        if (Math.abs(coord - endValue) < Math.abs(array[incrementIndex] - endValue)) {
+            return currentIndex
+        } else {
+            return incrementIndex
+        }
+    }, 0)
+    return buttonsCoordsX[whichY][coordIndex]
 }
 
-// for (let coord of buttonsCoords) {
-//     let coordsSquare = document.createElement('div')
-//     coordsSquare.classList.add('blue-test-circles')
-//     coordsSquare.style.left = coord.x + 'px'
-//     coordsSquare.style.top = coord.y + 'px'
-//     console.log(coordsSquare.style)
-//     document.querySelector('.buttons-area-container').appendChild(coordsSquare)
-
-//     // window.addEventListener('resize', (e) => {
-//     //     coordsSquare.style.left = coord.x + 'px'
-//     //     coordsSquare.style.top = coord.y + 'px'
-//     //     console.log('resize')
-//     // })
-// }
+function proccessSnapY(endValue) {
+    let coordIndex = buttonsCoordsY.reduce((incrementIndex, coord, currentIndex, array) => {
+        if (Math.abs(coord - endValue) < Math.abs(array[incrementIndex] - endValue)) {
+            return currentIndex
+        } else {
+            return incrementIndex
+        }
+    }, 0)
+    return coordIndex
+}
 
 Draggable.create('.draggable-remote', {
-    type: "x y",
+    type: "x, y",
     bounds: ".buttons-area-container",
     inertia: true,
-    edgeResistance: 0.65,
+    edgeResistance: 0.99,
+    throwResistance: 900,
+    onDrag: function (e) {
+        proccessSnapY(gsap.getProperty('.draggable-remote', 'y'))
+    },
+
+    onThrowComplete: function (e) {
+        isTouching(this)
+    },
     snap: {
-        x: function (endvalue) {
-            let coordIndex = buttonsCoordsX.reduce((incrementIndex, coord, currentIndex, array) => {
-                if (Math.abs(coord - endvalue) < Math.abs(array[incrementIndex] - endvalue)) {
-                    return currentIndex
-                } else {
-                    return incrementIndex
-                }
-            }, 0)
-            console.log(coordIndex)
-            return buttonsCoordsX[coordIndex]
+        x: function (endValue) {
+            whichY = proccessSnapY(gsap.getProperty('.draggable-remote', 'y'))
+            return proccessSnapX(endValue, whichY)
         },
-        y: function (endvalue) {
-            let coordIndex = buttonsCoordsY.reduce((incrementIndex, coord, currentIndex, array) => {
-                if (Math.abs(coord - endvalue) < Math.abs(array[incrementIndex] - endvalue)) {
-                    return currentIndex
-                } else {
-                    return incrementIndex
-                }
-            }, 0)
-            console.log(coordIndex)
-            return buttonsCoordsY[coordIndex]
-        }
+        y: function (endValue) {
+            return buttonsCoordsY[proccessSnapY(endValue)]
+        },
     }
 })
 
-
-function snapToNewButton(newButton) {
-    console.log(newButton)
+function isTouching(object) {
+    for (let button of buttons) {
+        if (object.hitTest(button)) {
+            let svgId = '#' + button.id.replace('button', 'svg')
+            changePrinciple(svgId)
+        }
+    }
 }
 
-
+function snapToNewButton(newButton) {
+    let tl = new TimelineMax()
+    tl.to('.draggable-remote', {
+        x: newButton.offsetLeft,
+        y: newButton.offsetTop,
+        ease: Bounce.easeOut,
+        duration: 1,
+    })
+    activeButton = newButton.id
+}
 
 function changePrinciple(newSvgId) {
     if (document.querySelector(newSvgId)) {
